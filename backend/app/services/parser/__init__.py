350ff3a2f7628a92b5eaeeeb47c94c678ce98e27
NiKhilForCybersec <nikhileswarreddy.c@gmail.com>
2025-12-26 02:46:08 -0500

    new

--- backend/app/services/parser/__init__.py
+++ backend/app/services/parser/__init__.py
@@ -6,7 +6,7 @@ Provides email parsing for EML, MSG formats and header analysis.
 
 from .eml_parser import parse_eml_file, parse_eml_bytes
 from .msg_parser import parse_msg_file, parse_msg_bytes
-from .header_analyzer import analyze_headers, HeaderAnalysisResult
+from .header_analyzer import analyze_headers
 from .qr_decoder import decode_qr_codes, QRCodeResult
 from .attachment_processor import process_attachments
 
@@ -16,7 +16,6 @@ __all__ = [
     'parse_msg_file',
     'parse_msg_bytes',
     'analyze_headers',
-    'HeaderAnalysisResult',
     'decode_qr_codes',
     'QRCodeResult',
     'process_attachments',
