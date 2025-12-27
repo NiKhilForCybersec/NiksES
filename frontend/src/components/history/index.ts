85de0e0557c01e04c37a71db06d1b2f28866917b
NiKhilForCybersec <nikhileswarreddy.c@gmail.com>
2025-12-26 21:00:13 -0500

    new

--- frontend/src/components/history/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * History Components
- * 
- * Re-exports all history-related components.
- */
-
-export { HistoryList } from './HistoryList';
-export { HistoryItem } from './HistoryItem';
-export { HistorySearch } from './HistorySearch';
